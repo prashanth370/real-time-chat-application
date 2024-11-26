a3bfc9288ea2a80d9178e8cf0d324de9c13515be
prashanth370 <banothprashanth2002@gmail.com>
2024-11-26 23:07:46 +0530

    first commit

---
+++ backend/src/seeds/user.seed.js
@@ -0,0 +1,55 @@
+import { config } from "dotenv";
+import { connectDB } from "../lib/db.js";
+import User from "../models/user.model.js";
+
+config();
+
+const seedUsers = [
+  // Female Users
+  {
+    email: "emma.thompson@example.com",
+    fullName: "Emma Thompson",
+    password: "123456",
+    profilePic: "https://randomuser.me/api/portraits/women/1.jpg",
+  },
+  {
+    email: "charlotte.williams@example.com",
+    fullName: "Charlotte Williams",
+    password: "123456",
+    profilePic: "https://randomuser.me/api/portraits/women/7.jpg",
+  },
+  {
+    email: "amelia.garcia@example.com",
+    fullName: "Amelia Garcia",
+    password: "123456",
+    profilePic: "https://randomuser.me/api/portraits/women/8.jpg",
+  },
+
+  // Male Users
+  {
+    email: "james.anderson@example.com",
+    fullName: "James Anderson",
+    password: "123456",
+    profilePic: "https://randomuser.me/api/portraits/men/1.jpg",
+  },
+  {
+    email: "daniel.rodriguez@example.com",
+    fullName: "Daniel Rodriguez",
+    password: "123456",
+    profilePic: "https://randomuser.me/api/portraits/men/7.jpg",
+  },
+];
+
+const seedDatabase = async () => {
+  try {
+    await connectDB();
+
+    await User.insertMany(seedUsers);
+    console.log("Database seeded successfully");
+  } catch (error) {
+    console.error("Error seeding database:", error);
+  }
+};
+
+// Call the function
+seedDatabase();
